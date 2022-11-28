e76db716e07002e15132e2e993fb04dfd97f697c
Svok2219 <shuvokoiri0@gmail.com>
2022-11-28 23:26:52 +0600

    come on champ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+// add bootstrap css 
+// import 'bootstrap/dist/css/bootstrap.css'
+// own css files here
+// import "../css/customcss.css";
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
